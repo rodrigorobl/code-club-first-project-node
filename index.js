d49225c7975ea34449b18cecc9ed5eff6c6e421f
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-02-21 13:42:05 -0300

    error

--- index.js
+++ index.js
@@ -42,13 +42,21 @@ app.get('/users', (request, response) => {
 
 
 app.post('/users', (request, response) => {
+try {
     const { name, age } = request.body
+
+    if(age < 18) throw new Error("Only allowed users over 18 years old")
 
     const user = { id: uuid.v4(), name, age }
 
     users.push(user)
 
     return response.status(201).json(user)
+} catch(err){
+    return response.status(400).json({error:err.message})
+} finally {
+    console.log("Terminou tudo")
+}
 })
 
 app.put('/users/:id', checkUserID, (request, response) => {
